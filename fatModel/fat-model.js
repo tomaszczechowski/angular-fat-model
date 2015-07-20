d175de57849ff46f19560acb28ea5d2263f8027b
Tomasz Czechowski <tomasz@czechowski.pl>
2015-07-20 20:59:24 +0200

    Translated tab to spaces - 2

--- fatModel/fat-model.js
+++ fatModel/fat-model.js
@@ -1,66 +1,66 @@
 (function () {
-	"use strict";
+  "use strict";
 
-	var app = angular.module('App.FatModel.Service', []);
+  var app = angular.module('App.FatModel.Service', []);
 
-	app.service('FatModelService', ['$rootScope', '$q', function ($rootScope, $q) {
-		var $scope = $rootScope.$new(true)
-      ,	smallModelsCollection = {};
+  app.service('FatModelService', ['$rootScope', '$q', function ($rootScope, $q) {
+    var $scope = $rootScope.$new(true)
+      , smallModelsCollection = {};
+  
+    var _action = function (name, groupName) {
+      var promisses = [];
 
-		var _action = function (name, groupName) {
-			var promisses = [];
+      $scope.$emit('FatModel:' + name + ':started');
 
-			$scope.$emit('FatModel:' + name + ':started');
+      for (var i in smallModelsCollection) {
+        var modelFeedback = smallModelsCollection[i][name](groupName);
 
-			for (var i in smallModelsCollection) {
-				var modelFeedback = smallModelsCollection[i][name](groupName);
+        if (modelFeedback) {
+          promisses.push(modelFeedback);
+        }
+      }
 
-				if (modelFeedback) {
-					promisses.push(modelFeedback);
-				}
-			}
+      $q.all(promisses).then(function() {
+        $scope.$emit('FatModel:' + name + ':finished');
+      });
+    };
 
-			$q.all(promisses).then(function() {
-				$scope.$emit('FatModel:' + name + ':finished');
-			});
-		};
+    var api = {
+      register: function (name, SmallModel) {
+        if ('fetch' in SmallModel && 'refresh' in SmallModel) {
+          return smallModelsCollection[name] = SmallModel;
+        }
 
-		var api = {
-			register: function (name, SmallModel) {
-				if ('fetch' in SmallModel && 'refresh' in SmallModel) {
-				  return smallModelsCollection[name] = SmallModel;
-				}
+        throw new Error('[FatModel:Error] - Small model needs to have implemented "fetch" and "refresh" method!');
+      },
 
-				throw new Error('[FatModel:Error] - Small model needs to have implemented "fetch" and "refresh" method!');
-			},
+      unRegister: function (name) {
+        if (name in smallModelsCollection) {
+          delete smallModelsCollection[name];
 
-			unRegister: function (name) {
-				if (name in smallModelsCollection) {
-					delete smallModelsCollection[name];
+          return true;
+        }
 
-					return true;
-				}
+        return false;
+      },
 
-				return false;
-			},
+      fetch: function () {
+        _action('fetch');
+      },
 
-			fetch: function () {
-				_action('fetch');
-			},
+      refresh: function () {
+        _action('refresh');
+      },
 
-			refresh: function () {
-				_action('refresh');
-			},
+      fetchGroup: function (groupName) {
+        _action('fetch', groupName);
+      },
 
-			fetchGroup: function (groupName) {
-				_action('fetch', groupName);
-			},
+      refreshGroup: function (groupName) {
+        _action('refresh', groupName);
+      }
+    };
 
-			refreshGroup: function (groupName) {
-				_action('refresh', groupName);
-			}
-		};
-
-		angular.extend(this, api);
-	}]);
+    angular.extend(this, api);
+  }]);
 })();
