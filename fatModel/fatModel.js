a80b93c0e1acf31a765fc140ef600cf0eb130bb0
Tomasz Czechowski <tomasz@czechowski.pl>
2015-07-25 14:36:31 +0200

    Added first part of jsDoc's

--- fatModel/fatModel.js
+++ fatModel/fatModel.js
@@ -1,33 +1,73 @@
+/**
+ * FatModel provider.
+ * Plugin to manage models in your application.
+ * {@link https://github.com/tomaszczechowski/angular-fat-model GitHub}.
+ *
+ * Copyright (c) 2015 Tomasz Czechowski
+ * Licensed under the MIT license.
+ */
+
 (function () {
   "use strict";
 
-  var app = angular.module('App.FatModel.Provider', []);
+  var app = angular.module('FatModelProvider', []);
 
+  /**
+   * FatModel provider definition.
+   */
   app.provider('$FatModel', function () {
 
-    var SmallModel = function (options, $rootScope, $timeout) {
+    /**
+     * Small model fasade.
+     *
+     * @param {object} Options - object contains model configuration.
+     * @param {object} $rootScope - Angular $rootScope object.
+     * @param {object} $timeout - Angular $timeout object.
+     *
+     * @return {object} - API of model.
+     */
+    var SmallModelFasade = function (options, $rootScope, $timeout) {
       var $scope = $rootScope.$new(true)
         , _options = angular.copy(options);
 
+      /**
+       * Method is in charge of sending events.
+       * Is used $emit method from Angular.
+       *
+       * @access private.
+       * @param {string} actionName - action name: fetch/refresh.
+       * @param {type} type - type of event: started/finised/error.
+       */
       var _sendEvent = function (actionName, type) {
         $scope.$emit('FatModel:' + actionName + ':' + type);
       };
 
-      var _action = function (actionName, groupName) {
-        var hasGroup = function (groupName) {
-          if (Array.isArray(groupName)) {
+      /**
+       * Method is in charge of fetching model.
+       * Calls methods success or error depending on result of fetching.
+       *
+       * @access private.
+       * @param {string} actionName - action name: fetch/refresh.
+       * @param {string|array} group - name of group or array of groups which has to be fetched or refreshed.
+       *
+       * @return {false|object} - method returns false when model doesn't belong to groups otherwise returns primise.
+       */
+      var _action = function (actionName, group) {
+
+        var hasGroup = function (group) {
+          if (Array.isArray(group)) {
             for (var i = 0; i < _options.groups.length; i++) {
-              if (groupName.indexOf(_options.groups[i]) !== -1) {
+              if (group.indexOf(_options.groups[i]) !== -1) {
                 return true;
               }
             }
             return false;
           } else {
-            return _options.groups.indexOf(groupName) !== -1;
+            return _options.groups.indexOf(group) !== -1;
           }
         };
 
-        if (!groupName || (groupName && hasGroup(groupName))) {
+        if (!group || (group && hasGroup(group))) {
           $timeout(function () {
             _sendEvent(actionName, 'started');
           });
@@ -47,13 +87,13 @@
       return {
         $on: angular.bind($scope, $scope.$on),
 
-        fetch: function (groupName) {
-          return _action('fetch', groupName);
+        fetch: function (group) {
+          return _action('fetch', group);
         },
 
-        refresh: function (groupName) {
+        refresh: function (group) {
           if (_options.refresh) {
-            return _action('refresh', groupName);
+            return _action('refresh', group);
           }
 
           return false;
@@ -71,7 +111,7 @@
         var $scope = $rootScope.$new(true)
           , modelsCollection = {};
 
-        var _action = function (actionName, groupName) {
+        var _action = function (actionName, group) {
           var promisses = [];
 
           $timeout(function () {
@@ -79,7 +119,7 @@
           });
 
           for (var i in modelsCollection) {
-            var modelFeedback = modelsCollection[i][actionName](groupName);
+            var modelFeedback = modelsCollection[i][actionName](group);
 
             if (modelFeedback) {
               promisses.push(modelFeedback);
@@ -106,7 +146,7 @@
 
             var _options = angular.extend($FatModelProvider.options, options);
 
-            return modelsCollection[options.name] = SmallModel(_options, $rootScope, $timeout);
+            return modelsCollection[options.name] = SmallModelFasade(_options, $rootScope, $timeout);
           },
 
           unRegister: function (name) {
@@ -135,12 +175,12 @@
             _action('refresh');
           },
 
-          fetchGroup: function (groupName) {
-            _action('fetch', groupName);
+          fetchGroup: function (group) {
+            _action('fetch', group);
           },
 
-          refreshGroup: function (groupName) {
-            _action('refresh', groupName);
+          refreshGroup: function (group) {
+            _action('refresh', group);
           }
         };
 
