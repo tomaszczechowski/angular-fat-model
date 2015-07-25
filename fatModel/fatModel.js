56505e589d3c2963442270e58c4ee38226a8c017
Tomasz Czechowski <tomasz@czechowski.pl>
2015-07-25 11:43:50 +0200

    Created small model

--- fatModel/fatModel.js
+++ fatModel/fatModel.js
@@ -5,6 +5,47 @@
 
   app.provider('$FatModel', function () {
 
+    var SmallModel = function (options, $rootScope) {
+      var $scope = $rootScope.$new(true)
+      var _options = angular.copy(options);
+
+      var _sendEvent = function (type) {
+        $scope.$emit('FatModel:' + _options.name + ':' + actionName + ':' + type);
+      };
+
+      var _action = function (actionName, groupName) {
+        if (groupName && _options.indexOf(groupName) !== -1) {
+          _sendEvent('started');
+
+          return _options.promise().then(function () {
+            options.success.call(options.success);
+            _sendEvent('finished');
+          },function () {
+            options.error.call(options.error);
+            _sendEvent('error');
+          });
+        }
+
+        return false;
+      };
+
+      return {
+        $on: angular.bind($scope, $scope.$on),
+
+        fetch: function (groupName) {
+          return _action('fetch', groupName);
+        },
+
+        refresh: function (groupName) {
+          if (_options.refresh) {
+            return _action('refresh', groupName);
+          }
+
+          return false;
+        }
+      }
+    };
+
     var $FatModelProvider = {
       options: {
         refresh: true,
@@ -15,13 +56,13 @@
         var $scope = $rootScope.$new(true)
           , smallModelsCollection = {};
 
-        var _action = function (name, groupName) {
+        var _action = function (actionName, groupName) {
           var promisses = [];
 
-          $scope.$emit('FatModel:' + name + ':started');
+          $scope.$emit('FatModel:' + actionName + ':started');
 
           for (var i in smallModelsCollection) {
-            var modelFeedback = smallModelsCollection[i][name](groupName);
+            var modelFeedback = smallModelsCollection[i][actionName](groupName);
 
             if (modelFeedback) {
               promisses.push(modelFeedback);
@@ -29,11 +70,13 @@
           }
 
           $q.all(promisses).then(function() {
-            $scope.$emit('FatModel:' + name + ':finished');
+            $scope.$emit('FatModel:' + actionName + ':finished');
           });
         };
 
         var $api = {
+          $on: angular.bind($scope, $scope.$on),
+
           register: function (options) {
             var requiredProperties = ['name', 'api', 'success', 'error'];
 
@@ -43,7 +86,9 @@
               }
             }
 
-            return smallModelsCollection[name] = angular.extend($FatModelProvider.options, options);
+            var _options = angular.extend($FatModelProvider.options, options);
+
+            return smallModelsCollection[name] = SmallModel(_options, $q, $rootScope);
           },
 
           unRegister: function (name) {
@@ -56,6 +101,13 @@
             return false;
           },
 
+          getSmallModel: function (name) {
+            if (name in smallModelsCollection) {
+              return smallModelsCollection[name];
+            }
+
+            throw new Error('[FatModel:getSmallModel:Error] - Model "' + name + '" does not exist.');
+          }
           fetch: function () {
             _action('fetch');
           },
