c34399ef813e06f63385a9514278e2bbb36cd974
Tomasz Czechowski <tomasz@czechowski.pl>
2015-09-26 14:54:43 +0200

    Added unit tests

--- test/specs/test.spec.js
+++ test/specs/test.spec.js
@@ -1,10 +1,11 @@
 describe("Fat Model", function () {
-  var $FatModelProvider;
+  var $FatModelProvider, $q;
 
   beforeEach(module('FatModelProvider'));
 
-  beforeEach(inject(function(_$FatModelProvider_) {
+  beforeEach(inject(function(_$FatModelProvider_, _$q_) {
     $FatModelProvider = _$FatModelProvider_;
+    $q = _$q_;
   }));
 
   describe('Test method "register"', function () {
@@ -42,6 +43,71 @@ describe("Fat Model", function () {
       };
 
       expect(getModel).toThrowError('[FatModel:getModel:Error] - Model "model-test" does not exist.');
+    });
+  });
+
+  describe('Test whether fat model runs proper methods at small models', function () {
+    it('Test whether fatModel register method throw error', function () {
+      var model = {
+        name: 'model-2',
+        groups: ['test'],
+        promise: function () {
+          return $q.defer().promise;
+        },
+        success: function () {},
+        error: function () {}
+      };
+
+      spyOn(model, 'promise').and.callThrough();
+
+      $FatModelProvider.register(model);
+      $FatModelProvider.fetchGroup('test');
+
+      expect(model.promise).toHaveBeenCalled();
+    });
+
+    it('Test whether models from the same group are run', function () {
+      beforeEach(function () {
+        var model_1 = {
+          name: 'model-1',
+          groups: ['group-1'],
+          promise: function () {
+            return $q.defer().promise;
+          },
+          success: function () {},
+          error: function () {}
+        };
+
+        var model_2 = {
+          name: 'model-2',
+          groups: ['group-2'],
+          promise: function () {
+            return $q.defer().promise;
+          },
+          success: function () {},
+          error: function () {}
+        };
+
+        spyOn(model_1, 'promise').and.callThrough();
+        spyOn(model_2, 'promise').and.callThrough();
+
+        $FatModelProvider.register(model_1);
+        $FatModelProvider.register(model_2);
+      });
+
+      it('Fetch group-1', function () {
+        $FatModelProvider.fetchGroup('group-1');
+
+        expect(model_1.promise).toHaveBeenCalled();
+        expect(model_2.promise).not.toHaveBeenCalled();
+      });
+
+      it('Fetch group-2', function () {
+        $FatModelProvider.fetchGroup('group-2');
+
+        expect(model_1.promise).not.toHaveBeenCalled();
+        expect(model_2.promise).toHaveBeenCalled();
+      });
     });
   });
 });
